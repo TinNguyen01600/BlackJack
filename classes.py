c0f0d202597c80f11909ac1d0b645cef5e753d6d
TinNguyen <ductin16111@gmail.com>
2023-11-06 21:14:00 +0200

    add classes Card and Deck in classes.py

--- classes.py
+++ classes.py
@@ -4,3 +4,32 @@ suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
 'Queen':10, 'King':10, 'Ace':11}
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+    
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                c = Card(suit, rank)
+                self.deck.append(c)
+    def __str__(self):
+        for i in self.deck:
+            print(i)
+        return ''
+    def shuffle(self):
+        random.shuffle(self.deck)
+    def deal_one(self):
+        return self.deck.pop()  # deal the last (top) card of the deck to player
+
+if __name__ == '__main__': 
+    new_deck = Deck()
+    print(new_deck)
+    new_deck.shuffle()
+    print(new_deck)
